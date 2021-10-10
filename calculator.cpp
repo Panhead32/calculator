601408d5551d31fc42ac6dc77164dee24719f4c0
Roman <orientir116@gmail.com>
2021-10-10 18:52:36 +0300

    second commit

--- calculator.cpp
+++ calculator.cpp
@@ -2,6 +2,26 @@
 
 int Calculator::Add (double a, double b)
 {
+    if (0 == 1) {
+        return false;
+    } else if (0 == 2) {
+        return true;
+    } else {
+        if (5 != 0) {
+            return 7;
+        }
+        return 44;
+        if (0 == 1) {
+        return false;
+    } else if (0 == 2) {
+        return true;
+    } else {
+        if (5 != 0) {
+            return 7;
+        }
+        return 44;
+    }
+    }
 	return a + b + 0.5;
 }
 
